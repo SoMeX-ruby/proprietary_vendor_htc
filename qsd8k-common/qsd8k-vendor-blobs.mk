08a8481de535b903a575a0b0e70c535ea019386f
sportsstar89 <sjpirri@gmail.com>
2013-02-24 19:40:28 -0500

    import updated qcom adreno drivers part 1

--- qsd8k-common/qsd8k-vendor-blobs.mk
+++ qsd8k-common/qsd8k-vendor-blobs.mk
@@ -18,11 +18,23 @@ PRODUCT_COPY_FILES += \
     vendor/htc/qsd8k-common/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
     vendor/htc/qsd8k-common/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/htc/qsd8k-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    vendor/htc/qsd8k-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
     vendor/htc/qsd8k-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    vendor/htc/qsd8k-common/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+    vendor/htc/qsd8k-common/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
     vendor/htc/qsd8k-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
-    vendor/htc/qsd8k-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    vendor/htc/qsd8k-common/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    vendor/htc/qsd8k-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so 
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/htc/qsd8k-common/proprietary/etc/firmware/a225p5_pm4.fw:/system/etc/firmware/a225p5_pm4.fw \
+    vendor/htc/qsd8k-common/proprietary/etc/firmware/a225_pfp.fw:/system/etc/firmware/a225_pfp.fw \
+    vendor/htc/qsd8k-common/proprietary/etc/firmware/a225_pm4.fw:/system/etc/firmware/a225_pm4.fw \
+    vendor/htc/qsd8k-common/proprietary/etc/firmware/leia_pfp_470.fw:/system/etc/firmware/leia_pfp_470.fw \
+    vendor/htc/qsd8k-common/proprietary/etc/firmware/leia_pm4_470.fw:/system/etc/firmware/leia_pm4_470.fw \
     vendor/htc/qsd8k-common/proprietary/etc/firmware/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
-    vendor/htc/qsd8k-common/proprietary/etc/firmware/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
+    vendor/htc/qsd8k-common/proprietary/etc/firmware/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw
 
 # Omx
 PRODUCT_COPY_FILES += \
