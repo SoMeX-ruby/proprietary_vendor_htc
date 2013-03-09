9368ad15c46b18a3bc3d39f00432f3ae23a98211
sportsstar89 <sjpirri@gmail.com>
2013-03-08 20:05:19 -0500

    Revert "update adreno to 4.2 release"
    This reverts commit 16b2bd3ff2fd043836670a01c506b8035c0752b9.

--- qsd8k-common/qsd8k-vendor-blobs.mk
+++ qsd8k-common/qsd8k-vendor-blobs.mk
@@ -14,32 +14,23 @@
 
 # Adreno
 PRODUCT_COPY_FILES += \
-    vendor/htc/qsd8k-common/proprietary/bin/gpu_dcvsd:system/bin/gpu_dcvsd \
     vendor/htc/qsd8k-common/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
     vendor/htc/qsd8k-common/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
     vendor/htc/qsd8k-common/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/htc/qsd8k-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/qsd8k-common/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
     vendor/htc/qsd8k-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
     vendor/htc/qsd8k-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-    vendor/htc/qsd8k-common/proprietary/lib/libc2d2_a3xx.so:system/lib/libc2d2_a3xx.so \
     vendor/htc/qsd8k-common/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     vendor/htc/qsd8k-common/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
-    vendor/htc/qsd8k-common/proprietary/lib/libCB.so:system/lib/libCB.so \
     vendor/htc/qsd8k-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
-    vendor/htc/qsd8k-common/proprietary/lib/libllvm-a3xx.so:system/lib/libllvm-a3xx.so \
-    vendor/htc/qsd8k-common/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
     vendor/htc/qsd8k-common/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
-    vendor/htc/qsd8k-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
-    vendor/htc/qsd8k-common/proprietary/lib/libsc-a3xx.so:system/lib/libsc-a3xx.so
+    vendor/htc/qsd8k-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so 
 
 # Firmware
 PRODUCT_COPY_FILES += \
     vendor/htc/qsd8k-common/proprietary/etc/firmware/a225p5_pm4.fw:/system/etc/firmware/a225p5_pm4.fw \
     vendor/htc/qsd8k-common/proprietary/etc/firmware/a225_pfp.fw:/system/etc/firmware/a225_pfp.fw \
     vendor/htc/qsd8k-common/proprietary/etc/firmware/a225_pm4.fw:/system/etc/firmware/a225_pm4.fw \
-    vendor/htc/qsd8k-common/proprietary/etc/firmware/a300_pfp.fw:/system/etc/firmware/a300_pfp.fw \
-    vendor/htc/qsd8k-common/proprietary/etc/firmware/a300_pm4.fw:/system/etc/firmware/a300_pm4.fw \
     vendor/htc/qsd8k-common/proprietary/etc/firmware/leia_pfp_470.fw:/system/etc/firmware/leia_pfp_470.fw \
     vendor/htc/qsd8k-common/proprietary/etc/firmware/leia_pm4_470.fw:/system/etc/firmware/leia_pm4_470.fw \
     vendor/htc/qsd8k-common/proprietary/etc/firmware/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
