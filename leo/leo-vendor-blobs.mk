66cec37c662eeb11348255866e99e758cba9f49e
sportsstar89 <sjpirri@gmail.com>
2013-03-13 16:30:13 -0400

    fix ril

--- leo/leo-vendor-blobs.mk
+++ leo/leo-vendor-blobs.mk
@@ -31,4 +31,5 @@ PRODUCT_COPY_FILES += \
   vendor/htc/leo/proprietary/lib/libhtc_ril.so:system/lib/libhtc_ril.so \
   vendor/htc/leo/proprietary/lib/libhtc_ril_wrapper.so:system/lib/libhtc_ril_wrapper.so \
   vendor/htc/leo/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-  vendor/htc/leo/proprietary/lib/librpc.so:system/lib/librpc.so
+  vendor/htc/leo/proprietary/lib/librpc.so:system/lib/librpc.so \
+  vendor/htc/leo/proprietary/lib/libril.so:system/lib/libril.so
