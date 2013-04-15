07f0ec3e0623c7341601833056744a6222797b12
sportsstar89 <sjpirri@gmail.com>
2013-04-14 20:48:35 -0400

    leo: remove bcm firmware

--- leo/leo-vendor-blobs.mk
+++ leo/leo-vendor-blobs.mk
@@ -33,7 +33,5 @@ PRODUCT_COPY_FILES += \
   vendor/htc/leo/proprietary/lib/libhtc_ril_wrapper.so:system/lib/libhtc_ril_wrapper.so \
   vendor/htc/leo/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
   vendor/htc/leo/proprietary/lib/librpc.so:system/lib/librpc.so \
-  vendor/htc/leo/proprietary/vendor/firmware/fw_bcm4329.bin:system/vendor/firmware/fw_bcm4329.bin \
-  vendor/htc/leo/proprietary/vendor/firmware/fw_bcm4329_apsta.bin:system/vendor/firmware/fw_bcm4329_apsta.bin \
   vendor/htc/leo/proprietary/bin/wpa_supplicant:system/bin/wpa_supplicant \
   vendor/htc/leo/proprietary/bin/wpa_cli:system/bin/wpa_cli
